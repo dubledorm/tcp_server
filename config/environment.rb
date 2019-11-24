53aa8458388c2fe520ba68ef1f263e88062ffece
Aleksey Aleshin <dubledorm@yandex.ru>
2019-11-24 21:17:38 +0300

    swagger API

--- config/environment.rb
+++ config/environment.rb
@@ -7,10 +7,5 @@ Rails.application.initialize!
 $tcp_server_control = TcpServer::TcpServerControl.new
 $tcp_server_control.start([3001, 3002])
 
-# Thread.new do
-#   loop do
-#     $tcp_server_control.start_pair([3001, 3002])
-#     sleep(1) while $tcp_server_control.pair_alive?
-#   end
-# end
+
 
